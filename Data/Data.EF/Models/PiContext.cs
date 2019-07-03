6bdfdf00db04bc43ba44787f6da578e291807cfa
Damian <flosbox@gmail.com>
2019-07-03 23:27:12 +0200

    [Persistence] fix context

--- Data/Data.EF/Models/PiContext.cs
+++ Data/Data.EF/Models/PiContext.cs
@@ -45,6 +45,194 @@ namespace Data.EF.Models
         {
             modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
 
+            modelBuilder.Entity<AdditionalInfo>(entity =>
+            {
+                entity.ToTable("AdditionalInfo", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Flat>(entity =>
+            {
+                entity.ToTable("Flat", "otodom");
+
+                entity.HasIndex(e => e.OtoDomId)
+                    .HasName("Flat_OtodomId_IDX");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.OtoDomId)
+                    .IsRequired()
+                    .HasMaxLength(7);
+
+                entity.Property(e => e.Rent).HasColumnType("smallmoney");
+
+                entity.Property(e => e.Surface).HasColumnType("decimal(9, 2)");
+
+                entity.Property(e => e.TotalPrice).HasColumnType("money");
+
+                entity.Property(e => e.Url)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.FormOfProperty)
+                    .WithMany(p => p.Flat)
+                    .HasForeignKey(d => d.FormOfPropertyId)
+                    .HasConstraintName("FK_FormOfProperty_Flat");
+
+                entity.HasOne(d => d.Heating)
+                    .WithMany(p => p.Flat)
+                    .HasForeignKey(d => d.HeatingId)
+                    .HasConstraintName("FK_Heating_Flat");
+
+                entity.HasOne(d => d.Location)
+                    .WithMany(p => p.Flat)
+                    .HasForeignKey(d => d.LocationId)
+                    .HasConstraintName("FK_Location_Flat");
+
+                entity.HasOne(d => d.Market)
+                    .WithMany(p => p.Flat)
+                    .HasForeignKey(d => d.MarketId)
+                    .HasConstraintName("FK_Market_Flat");
+
+                entity.HasOne(d => d.Type)
+                    .WithMany(p => p.Flat)
+                    .HasForeignKey(d => d.TypeId)
+                    .HasConstraintName("FK_TypeOfBuilding_Flat");
+            });
+
+            modelBuilder.Entity<FlatAdditionalInfo>(entity =>
+            {
+                entity.ToTable("FlatAdditionalInfo", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.HasOne(d => d.AdditionalInfo)
+                    .WithMany(p => p.FlatAdditionalInfo)
+                    .HasForeignKey(d => d.AdditionalInfoId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_AdditionalInfo_FlatAdditionalInfo");
+
+                entity.HasOne(d => d.Flat)
+                    .WithMany(p => p.FlatAdditionalInfo)
+                    .HasForeignKey(d => d.FlatId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Flat_FlatAdditionalInfo");
+            });
+
+            modelBuilder.Entity<FlatCategoty>(entity =>
+            {
+                entity.ToTable("FlatCategoty", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<FlatParent>(entity =>
+            {
+                entity.ToTable("FlatParent", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.AvgPrice).HasColumnType("money");
+
+                entity.Property(e => e.AvgPricePerMeter).HasColumnType("money");
+
+                entity.Property(e => e.DateFetched).HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<FlatSeries>(entity =>
+            {
+                entity.ToTable("FlatSeries", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.AvgPrice).HasColumnType("money");
+
+                entity.Property(e => e.AvgPricePerMeter).HasColumnType("money");
+
+                entity.Property(e => e.DateFetched).HasColumnType("datetime");
+
+                entity.HasOne(d => d.BestValue)
+                    .WithMany(p => p.FlatSeriesBestValue)
+                    .HasForeignKey(d => d.BestValueId)
+                    .HasConstraintName("FK_BestValue_FlatSeries");
+
+                entity.HasOne(d => d.Biggest)
+                    .WithMany(p => p.FlatSeriesBiggest)
+                    .HasForeignKey(d => d.BiggestId)
+                    .HasConstraintName("FK_Biggest_FlatSeries");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.FlatSeries)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK_Category_FlatSeries");
+
+                entity.HasOne(d => d.Cheapest)
+                    .WithMany(p => p.FlatSeriesCheapest)
+                    .HasForeignKey(d => d.CheapestId)
+                    .HasConstraintName("FK_Cheapest_FlatSeries");
+
+                entity.HasOne(d => d.MostExpensive)
+                    .WithMany(p => p.FlatSeriesMostExpensive)
+                    .HasForeignKey(d => d.MostExpensiveId)
+                    .HasConstraintName("FK_MostExpensive_FlatSeries");
+
+                entity.HasOne(d => d.Smallest)
+                    .WithMany(p => p.FlatSeriesSmallest)
+                    .HasForeignKey(d => d.SmallestId)
+                    .HasConstraintName("FK_Smallest_FlatSeries");
+            });
+
+            modelBuilder.Entity<FormOfProperty>(entity =>
+            {
+                entity.ToTable("FormOfProperty", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Heating>(entity =>
+            {
+                entity.ToTable("Heating", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Location>(entity =>
+            {
+                entity.ToTable("Location", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Market>(entity =>
+            {
+                entity.ToTable("Market", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
             modelBuilder.Entity<Series>(entity =>
             {
                 entity.ToTable("Series", "binance");
@@ -71,6 +259,17 @@ namespace Data.EF.Models
                 entity.Property(e => e.FetchedDate).HasColumnType("datetime");
 
                 entity.Property(e => e.Total).HasColumnType("money");
+            });
+
+            modelBuilder.Entity<TypeOfBuilding>(entity =>
+            {
+                entity.ToTable("TypeOfBuilding", "otodom");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
             });
         }
     }
