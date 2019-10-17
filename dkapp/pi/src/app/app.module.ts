358f6ac656587e58a7a24517e8488363f9dab81c
Damian <flosbox@gmail.com>
2019-10-17 23:26:46 +0200

    Add font awesome

--- dkapp/pi/src/app/app.module.ts
+++ dkapp/pi/src/app/app.module.ts
@@ -5,19 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatGridListModule, 
-  MatToolbarModule, 
-  MatSidenavModule, 
-  MatFormFieldModule, 
-  MatButtonModule, 
-  MatInputModule, 
-  MatIconModule,
-  MatSelectModule } 
-  from '@angular/material';
 import { PiFooterComponent } from './pi-footer/pi-footer.component';
 import { PiNavbarComponent } from './pi-navbar/pi-navbar.component';
 import { PiContentComponent } from './pi-content/pi-content.component';
 import { PiBtnComponent } from './controls/pi-btn/pi-btn.component';
+import { PiLineChartComponent } from './controls/pi-line-chart/pi-line-chart.component';
 
 
 @NgModule({
@@ -27,21 +19,14 @@ import { PiBtnComponent } from './controls/pi-btn/pi-btn.component';
     PiContentComponent,
     PiFooterComponent,
     PiBtnComponent,
+    PiLineChartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatGridListModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatIconModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
